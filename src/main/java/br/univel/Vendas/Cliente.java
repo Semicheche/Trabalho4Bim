e67ba506dae7c3dd6a66f560389e09e8baa1af23
Semicheche <lucianosemicheche@gmail.com>
2015-10-27 21:11:08 -0200

    criação das enum´s e classes

---
+++ src/main/java/br/univel/Vendas/Cliente.java
@@ -0,0 +1,58 @@
+package br.univel.Vendas;
+
+public class Cliente {
+
+	
+	private int id;
+	private String nome;
+	private String telefone;
+	private String endereco;
+	private String cidade;
+	private Estado estado;
+	private Genero genero;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+}
---
+++ src/main/java/br/univel/Vendas/Genero.java
@@ -0,0 +1,8 @@
+package br.univel.Vendas;
+
+public enum Genero {
+	
+	M, //masculino
+	F, //Feminino
+
+}
