9d2752850bfe86284eabfcc1b1e5f0b6773552bd
Semicheche <lucianosemicheche@gmail.com>
2015-10-28 13:34:07 -0200

    Criação tela Principal

---
+++ src/main/java/br/univel/Vendas/TelaPrincipal.java
@@ -0,0 +1,57 @@
+package br.univel.Vendas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Cadastro");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmProduto = new JMenuItem("Produto");
+		mnNewMenu.add(mntmProduto);
+		
+		JMenuItem mntmCliente = new JMenuItem("Cliente");
+		mnNewMenu.add(mntmCliente);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
