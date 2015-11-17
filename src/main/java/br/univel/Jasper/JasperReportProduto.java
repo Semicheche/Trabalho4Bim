5a29698b169d21356d920e7ce8690bb1b47c3eb8
Semicheche <lucianosemicheche@gmail.com>
2015-11-17 16:56:13 -0200

    relatorio produto jasper

---
+++ src/main/java/br/univel/Jasper/JasperReportProduto.java
@@ -0,0 +1,43 @@
+package br.univel.Jasper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.univel.Vendas.Cliente;
+import br.univel.Vendas.Conexao;
+import br.univel.Vendas.ModelCliente;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class JasperReportProduto {
+
+	Conexao con = new Conexao();
+
+	private String arq = "C:\\Users\\luciano\\JaspersoftWorkspace\\MyReports\\Produto.jasper";
+
+	public JasperReportProduto() {
+		JasperPrint jasperPrint = null;
+
+		try {
+			
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("Relatorio: ", Cliente.class.getName());
+			
+
+			jasperPrint = JasperFillManager.fillReport(arq, null,
+					con.getConnection());
+			JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+			jasperViewer.setVisible(true);
+		} catch (JRException ex) {
+			ex.printStackTrace();
+		}
+		
+		new ModelCliente();
+	}
+	
+	
+
+}
